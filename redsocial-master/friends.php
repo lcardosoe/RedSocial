83924f821dadcc62fb2870636f94dccb2cf5e9df
CAGARZONRO <71109754+CAGARZONRO@users.noreply.github.com>
2020-10-13 18:14:59 -0500

    Update friends.php

--- redsocial-master/friends.php
+++ redsocial-master/friends.php
@@ -1,223 +1,235 @@
-<?php include('header.php'); ?>    
-<?php include('session.php'); ?>    
-    <body>
-	<?php include('navbar.php'); ?>
-			<div id="masthead">  
-			<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img class="d-block w-100 img-responsive" src="images/foto.png" alt="First slide">
-    </div>
-    
-  </div>
-</div>
-				<div class="container">
-					<?php include('heading.php'); ?>
-					
-				</div><!-- /cont -->
-				<div class="container">
-					<div class="row">
-					<div class="col-md-12">
-						<div class="top-spacer"> </div>
-					</div>
-					</div> 
-				</div><!-- /cont -->
-			</div>
-<div class="container">
-  <div class="row">
-    <div class="col-md-12"> 
-      <div class="panel">
-        <div class="panel-body">
-          <!--/stories-->
-          <div class="row">    
+<?php include('header.php'); ?>
+<?php include('session.php'); ?>
 
+<body>
+  <style>
+    .hover01 figure img {
+      -webkit-transform: scale(1);
+      transform: scale(1);
+      -webkit-transition: .3s ease-in-out;
+      transition: .3s ease-in-out;
+    }
 
-	
-<div class="row">
-<div class="col-md-12">
-<div class="x_panel">
-<div class="x_title">
-<h2>  Amigos <small> Presione en el nombre de su amigo para ir al perfil</small></h2>
-<ul class="nav navbar-right panel_toolbox">
-<li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
-</li>
-<li class="dropdown">
-<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
-<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-</div>
-</li>
-<li><a class="close-link"><i class="fa fa-close"></i></a>
-</li>
-</ul>
-<div class="clearfix"></div>
-</div>
-<div class="x_content">
-<div class="row">
-<div class="col-md-2">
-<div class="thumbnail">
-<div class="image view view-first">
-<img style="width: 100%; display: block;" src="images/male5.jpg" alt="image">
-<div class="mask">
-<label><p><a href="#" class="alert-link">Falcao Garcia</a></p></label>
-<div class="tools tools-bottom">
-</div>
-</div>
-</div>
-<div class="caption">
-<p>El tigre</p>
-</div>
-</div>
-</div>
-<div class="col-md-2">
-<div class="thumbnail">
-<div class="image view view-first">
-<img style="width: 100%; display: block;" src="images/male5.jpg" alt="image">
-<div class="mask" >
-<label><p><a href="#" class="alert-link">Maluma</a></p></label>
-<div class="tools tools-bottom">
-<a href="#"><i class="fa fa-link"></i></a>
-<a href="#"><i class="fa fa-pencil"></i></a>
-<a href="#"><i class="fa fa-times"></i></a>
-</div>
-</div>
-</div>
-<div class="caption">
-<p>Pretty boy</p>
-</div>
-</div>
-</div>
-<div class="col-md-2">
-<div class="thumbnail">
-<div class="image view view-first">
-<img style="width: 100%; display: block;" src="images/male5.jpg" alt="image">
-<div class="mask">
-<label><p><a href="#" class="alert-link">Leonardo</a></p></label>
-<div class="tools tools-bottom">
-<a href="#"><i class="fa fa-link"></i></a>
-<a href="#"><i class="fa fa-pencil"></i></a>
-<a href="#"><i class="fa fa-times"></i></a>
-</div>
-</div>
-</div>
-<div class="caption">
-<p>Musicaa</p>
-</div>
-</div>
-</div>
-<div class="col-md-2">
-<div class="thumbnail">
-<div class="image view view-first">
-<img style="width: 100%; display: block;" src="images/male5.jpg" alt="image">
-<div class="mask">
-<label><p><a href="#" class="alert-link">El dueño del chuzo</a></p></label>
-<div class="tools tools-bottom">
-<a href="#"><i class="fa fa-link"></i></a>
-<a href="#"><i class="fa fa-pencil"></i></a>
-<a href="#"><i class="fa fa-times"></i></a>
-</div>
-</div>
-</div>
-<div class="caption">
-<p>Ser exitoso</p>
-</div>
-</div>
-</div>
-<div class="col-md-2">
-<div class="thumbnail">
-<div class="image view view-first">
-<img style="width: 100%; display: block;" src="images/male6.jpg" alt="image">
-<div class="mask">
-<label><p><a href="#" class="alert-link">Casi nadie</a></p></label>
-<div class="tools tools-bottom">
-<a href="#"><i class="fa fa-link"></i></a>
-<a href="#"><i class="fa fa-pencil"></i></a>
-<a href="#"><i class="fa fa-times"></i></a>
-</div>
-</div>
-</div>
-<div class="caption">
-<p>Ok</p>
-</div>
-</div>
-</div>
-<div class="col-md-2">
-<div class="thumbnail">
-<div class="image view view-first">
-<img style="width: 100%; display: block;" src="images/male6.jpg" alt="image">
-<div class="mask">
-<label><p><a href="#" class="alert-link">El che</a></p></label>
-<div class="tools tools-bottom">
-<a href="#"><i class="fa fa-link"></i></a>
-<a href="#"><i class="fa fa-pencil"></i></a>
-<a href="#"><i class="fa fa-times"></i></a>
-</div>
-</div>
-</div>
-<div class="caption">
-<p>Revolucion</p>
-</div>
-</div>
-</div>
-<div class="col-md-2">
-<div class="thumbnail">
-<div class="image view view-first">
-<img style="width: 100%; display: block;" src="images/male6.jpg" alt="image">
-<div class="mask">
-<label><p><a href="#" class="alert-link">Don omar</a></p></label>
-<div class="tools tools-bottom">
-<a href="#"><i class="fa fa-link"></i></a>
-<a href="#"><i class="fa fa-pencil"></i></a>
-<a href="#"><i class="fa fa-times"></i></a>
-</div>
-</div>
-</div>
-<div class="caption">
-<p>1 2 3 4</p>
-</div>
-</div>
-</div>
-<div class="col-md-2">
-<div class="thumbnail">
-<div class="image view view-first">
-<img style="width: 75%; display: block;" src="images/fotoperfil.jpg" alt="image">
-<div class="mask">
-<label><p><a href="#" class="alert-link">Ivan Duque</a></p></label>
-<div class="tools tools-bottom">
-<a href="#"><i class="fa fa-link"></i></a>
-<a href="#"><i class="fa fa-pencil"></i></a>
-<a href="#"><i class="fa fa-times"></i></a>
-</div>
-</div>
-</div>
-<div class="caption">
-<p>Hey there</p>
-</div>
-</div>
-</div>
+    .hover01 figure:hover img {
+      -webkit-transform: scale(1.3);
+      transform: scale(1.3);
+    }
+  </style>
+  <?php include('navbar.php'); ?>
 
-
-</div>
-</div>
-</div>
-</div>
-</div>
-	
+  <div class="container">
+    <div id="masthead">
+      <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img class="d-block w-100 img-responsive" src="images/foto.png" alt="First slide">
           </div>
-          <hr>
-		</div>
-		
+
+        </div>
       </div>
-                                                                                 
-	                                                
-                                                      
-	   </div><!--/col-12-->
-	   
+    </div><!-- /cont -->
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+        </div>
+      </div>
+    </div><!-- /cont -->
   </div>
-</div>
-		   
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="panel">
+          <div class="panel-body">
+            <!--/stories-->
+            <div class="row">
 
 
-<?php include('footer.php'); ?>
-        
-    </body>
+
+              <div class="row">
+                <div class="col-md-12">
+                  <div class="x_panel">
+                    <div class="x_title">
+                      <h2>&nbsp; Amigos <small> Encuentra aqui a tus amigos</small></h2>
+                      <ul class="nav navbar-right panel_toolbox">
+                        <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+                        </li>
+                        <li class="dropdown">
+                          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
+                          <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                          </div>
+                        </li>
+                        <li><a class="close-link"><i class="fa fa-close"></i></a>
+                        </li>
+                      </ul>
+                      <div class="clearfix"></div>
+                    </div>
+                    <div class="x_content">
+                      <div class="row">
+                        <div class="hover01 column">
+                          <div class="col-md-2">
+                            <div class="thumbnail">
+                              <div class="image view view-first">
+                                <a href="profile.php">
+                                  <figure><img style="width: 100%; display: block;" src="images/lion.jpg" alt="image"></figure>
+                                  <div class="mask">
+                                    <label>
+                                      <p>
+                                        <class="alert-link">Falcao Garcia
+                                      </p>
+                                    </label>
+                                    <div class="tools tools-bottom">
+                                    </div>
+                                  </div>
+                              </div>
+                              <div class="caption">
+                                <p>El tigre</p></a>
+                              </div>
+                            </div>
+                          </div>
+                          <div class="col-md-2">
+                            <div class="thumbnail">
+                              <div class="image view view-first">
+                                <a href="profile.php">
+                                  <figure><img style="width: 100%; display: block;" src="images/male5.jpg" alt="image"></figure>
+                                  <div class="mask">
+                                    <label>
+                                      <p>
+                                        <class="alert-link">Maluma
+                                      </p>
+                                    </label>
+                                  </div>
+                              </div>
+                              <div class="caption">
+                                <p>Pretty boy</p></a>
+                              </div>
+                            </div>
+                          </div>
+                          <div class="col-md-2">
+                            <div class="thumbnail">
+                              <div class="image view view-first">
+                                <a href="profile.php">
+                                  <figure><img style="width: 100%; display: block;" src="images/he.jpg" alt="image"></figure>
+                                  <div class="mask">
+                                    <label>
+                                      <p>
+                                        <class="alert-link">Leonardo
+                                      </p>
+                                    </label>
+                                  </div>
+                              </div>
+                              <div class="caption">
+                                <p>Musicaa</p></a>
+                              </div>
+                            </div>
+                          </div>
+                          <div class="col-md-2">
+                            <div class="thumbnail">
+                              <div class="image view view-first">
+                                <a href="profile.php">
+                                  <figure><img style="width: 100%; display: block;" src="images/male5.jpg" alt="image"></figure>
+                                  <div class="mask">
+                                    <label>
+                                      <p>
+                                        <class="alert-link">El dueño
+                                      </p>
+                                    </label>
+                                  </div>
+                              </div>
+                              <div class="caption">
+                                <p>Ser exitoso</p></a>
+                              </div>
+                            </div>
+                          </div>
+                          <div class="col-md-2">
+                            <div class="thumbnail">
+                              <div class="image view view-first">
+                              <a href="profile.php">
+                                <figure><img style="width: 100%; display: block;" src="images/male6.jpg" alt="image">
+                                  <figure>
+                                    <div class="mask">
+                                      <label>
+                                        <p><class="alert-link">Casi nadie</p>
+                                      </label>
+                                    </div>
+                              </div>
+                              <div class="caption">
+                                <p>Ok</p></a>
+                              </div>
+                            </div>
+                          </div>
+                          <div class="col-md-2">
+                            <div class="thumbnail">
+                              <div class="image view view-first">
+                              <a href="profile.php">
+                                <figure><img style="width: 100%; display: block;" src="images/male6.jpg" alt="image"></figure>
+                                <div class="mask">
+                                  <label>
+                                    <p><class="alert-link">El che</p>
+                                  </label>
+                                </div>
+                              </div>
+                              <div class="caption">
+                                <p>Revolucion</p></a>
+                              </div>
+                            </div>
+                          </div>
+                          <div class="col-md-2">
+                            <div class="thumbnail">
+                              <div class="image view view-first">
+                              <a href="profile.php">
+                                <figure><img style="width: 100%; display: block;" src="images/omar.jpg" alt="image"></figure>
+                                <div class="mask">
+                                  <label>
+                                    <p><class="alert-link">Don omar</p>
+                                  </label>
+                                </div>
+                              </div>
+                              <div class="caption">
+                                <p>1 2 3 4</p></a>
+                              </div>
+                            </div>
+                          </div>
+                          <div class="col-md-2">
+                            <div class="thumbnail">
+                              <div class="image view view-first">
+                              <a href="profile.php">
+                                <figure><img style="width: 84%; display: block;" src="images/ester.jpg" alt="image"></figure>
+                                <div class="mask">
+                                  <label>
+                                    <p><class="alert-link">Edwin</p>
+                                  </label>
+                                </div>
+                              </div>
+                              <div class="caption">
+                                <p>Hey there</p></a>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+            <hr>
+          </div>
+
+        </div>
+
+
+
+      </div>
+      <!--/col-12-->
+
+    </div>
+  </div>
+
+
+
+  <?php include('footer.php'); ?>
+
+</body>
+
 </html>
